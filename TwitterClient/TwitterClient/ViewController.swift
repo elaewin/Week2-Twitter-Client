ee34fb7a04da1782eef47c9e5ceadce769a532c0
Erica Winberry <elaewin@gmail.com>
2017-03-21 15:48:20 -0700

    get rid of extraneous method.

--- TwitterClient/TwitterClient/ViewController.swift
+++ TwitterClient/TwitterClient/ViewController.swift
@@ -48,13 +48,7 @@ class ViewController: UIViewController {
                 }
             }
         }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-        
+    }        
 }
 
 // MARK: TableViewDataSource and TableViewDelegate methods

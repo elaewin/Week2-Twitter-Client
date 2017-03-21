433152a4ee7b53eea20664f42dfd65f126743b8c
Erica Winberry <elaewin@gmail.com>
2017-03-21 09:21:49 -0700

    removed unnecessary didReceiveMemoryWarning method. Not going to use it.

--- TwitterClient/TwitterClient/ViewController.swift
+++ TwitterClient/TwitterClient/ViewController.swift
@@ -40,12 +40,6 @@ class ViewController: UIViewController {
             }
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
